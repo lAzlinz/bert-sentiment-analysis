5cf5c0a31f9d7250881ab0eae2869a64a1e472e5
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-05-11 23:28:06 +0800

    split the dataset into train and val set

--- different_trainer.py
+++ different_trainer.py
@@ -3,5 +3,10 @@ os.environ['TF_USE_LEGACY_KERAS'] = '1'
 import ktrain
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('./datasets/100kDataset_withSentiment.csv')
+
+classes = list(set(df.sentiment))
+
+X_train, X_test, y_train, y_test = train_test_split(df.headline, df.sentiment, test_size=0.2, random_state=42)
