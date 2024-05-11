ae8312ff1983bfdb22cb7c680b140a2f471fc851
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-05-11 23:35:05 +0800

    preprocess the dataset

--- different_trainer.py
+++ different_trainer.py
@@ -1,6 +1,6 @@
 import os
 os.environ['TF_USE_LEGACY_KERAS'] = '1'
-import ktrain
+from ktrain import text
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -10,3 +10,17 @@ df = pd.read_csv('./datasets/100kDataset_withSentiment.csv')
 classes = list(set(df.sentiment))
 
 X_train, X_test, y_train, y_test = train_test_split(df.headline, df.sentiment, test_size=0.2, random_state=42)
+
+# preprocess
+trn, val, preproc = text.texts_from_array(
+    x_train=X_train.values,
+    y_train=y_train.values,
+    x_test=X_test.values,
+    y_test=y_test.values,
+    class_names=classes,
+    val_pct=0.1, 
+    max_features=30000, 
+    maxlen=350,
+    preprocess_mode='distilbert',
+    ngram_range=1
+)
