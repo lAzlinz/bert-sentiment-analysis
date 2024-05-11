70c84c5f09e00f9c0f6469f9c4d4cd9acd017889
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-05-11 18:42:41 +0800

    get the train and test sets

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from datasets import load_dataset
 
 
 imdb = load_dataset('imdb')
+
+# only take a small part of the big imdb dataset
+small_train_dataset = imdb['train'].shuffle(seed=42).select([i for i in list(range(3_000))])
+small_test_dataset = imdb['test'].shuffle(seed=42).select([i for i in list(range(300))])
