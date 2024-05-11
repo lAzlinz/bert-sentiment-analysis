5fd1eb2a4a8bf30857bac80d0bfaa71db3126e3e
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-05-11 18:47:42 +0800

    tokenize the datasets

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ small_train_dataset = imdb['train'].shuffle(seed=42).select([i for i in list(ran
 small_test_dataset = imdb['test'].shuffle(seed=42).select([i for i in list(range(300))])
 
 tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+
+def preprocess_function(example):
+    return tokenizer(example['text'], truncation=True)
+
+tokenized_train = small_train_dataset.map(preprocess_function, batched=True)
+tokenized_test = small_test_dataset.map(preprocess_function, batched=True)
